47a0ad3434811c785ed4665cde5ea54731d5aa5b
FatinFadillah <115238757+FatinFadillah@users.noreply.github.com>
2023-01-04 18:06:59 +0700

    Add files via upload

---
+++ Pertemuan1/percabangan.py
@@ -0,0 +1,8 @@
+num = eval(input("Masukkan integer: "))
+
+if num > 0:
+    print("Bilangan Positif")
+elif num == 0:
+    print("Bilangan nol")
+else:
+    print("Bilangan Negatif")
---
+++ Pertemuan1/variable.py
@@ -0,0 +1,7 @@
+namaLengkap = "Fatin Fadillah" #  string
+asal = "Bima" # string
+umur = 20 # integer
+hobi = "Nonton" # string
+isStudent = True # boolean
+
+print('Hallo nama saya', namaLengkap)
